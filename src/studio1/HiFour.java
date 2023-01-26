a9f86a7da5d34c1fe015c409b49dd16fdc2b02da
rylalkoroma <rylalkoroma@localhost>
2023-01-26 13:28:14 -0600

    done

--- src/studio1/HiFour.java
+++ src/studio1/HiFour.java
@@ -9,17 +9,17 @@ import java.util.Scanner;
 public class HiFour {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		System.out.print("Enter value for arg 0: ");
+		System.out.println("What is your name?");
 		String s0 = in.nextLine();
-		System.out.print("Enter value for arg 1: ");
+		System.out.println("What is your name?");
 		String s1 = in.nextLine();
-		System.out.print("Enter value for arg 2: ");
+		System.out.println("What is your name?");
 		String s2 = in.nextLine();
-		System.out.print("Enter value for arg 3: ");
+		System.out.println("What is your name?");
 		String s3 = in.nextLine();
 		//
 		// Say hello to the names in s0 through s3.
 		//
-
+		System.out.println("Greetings " + s0 + ", " + s1 + ", " + s2 + ", and " + s3 + "!");
 	}
 }
